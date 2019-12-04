7e3549d96db45b74c56b22e401a59961357dd211
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-04 14:09:53 +0200

    убрал весь клиент сайд. будем пилить фронт на ангуларе

---
+++ src/main/java/oleksandrdiachenko/pricechecker/controller/PriceRestController.java
@@ -0,0 +1,27 @@
+package oleksandrdiachenko.pricechecker.controller;
+
+import oleksandrdiachenko.pricechecker.service.PriceService;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PriceRestController {
+
+    @Autowired
+    private PriceService priceService;
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping("/price-table/{urlColumnNumber}/{insertColumnNumber}")
+    public List<List<String>> getPriceTable(@PathVariable Integer urlColumnNumber,
+                                            @PathVariable Integer insertColumnNumber)
+            throws IOException, InvalidFormatException {
+        return priceService.buildPriceTable("./price.xlsx", urlColumnNumber, insertColumnNumber);
+    }
+}
