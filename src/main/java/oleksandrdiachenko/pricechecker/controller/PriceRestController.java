1856f9b392d7385d508ecb02ca5fb2e4d8f4d1a9
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-09 17:50:04 +0200

    добавил загрузку файла на сервер

--- src/main/java/oleksandrdiachenko/pricechecker/controller/PriceRestController.java
+++ src/main/java/oleksandrdiachenko/pricechecker/controller/PriceRestController.java
@@ -3,11 +3,13 @@ package oleksandrdiachenko.pricechecker.controller;
 import oleksandrdiachenko.pricechecker.service.PriceService;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 @RestController
@@ -16,10 +18,17 @@ public class PriceRestController {
     @Autowired
     private PriceService priceService;
 
-    @RequestMapping("/api/price-table/{urlColumnNumber}/{insertColumnNumber}")
+    @GetMapping("/api/price-table/{urlColumnNumber}/{insertColumnNumber}")
     public List<List<String>> getPriceTable(@PathVariable Integer urlColumnNumber,
                                             @PathVariable Integer insertColumnNumber)
             throws IOException, InvalidFormatException {
         return priceService.buildPriceTable("./file/price.xlsx", urlColumnNumber, insertColumnNumber);
+    }
+
+    @PostMapping("/api/file-upload")
+    public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        try (OutputStream out = new FileOutputStream(new File("./file/price.xlsx"))) {
+            out.write(file.getBytes());
+        }
     }
 }
