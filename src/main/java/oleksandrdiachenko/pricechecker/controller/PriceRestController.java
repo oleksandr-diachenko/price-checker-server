feb6b52b205458ab105213f3c0b943bdabef6718
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-23 13:49:15 +0200

    добавил метод в контроллер и тесты

--- src/main/java/oleksandrdiachenko/pricechecker/controller/PriceRestController.java
+++ src/main/java/oleksandrdiachenko/pricechecker/controller/PriceRestController.java
@@ -1,10 +1,13 @@
 package oleksandrdiachenko.pricechecker.controller;
 
+import lombok.SneakyThrows;
+import oleksandrdiachenko.pricechecker.service.MainService;
 import oleksandrdiachenko.pricechecker.service.PriceCheckService;
 import oleksandrdiachenko.pricechecker.util.WorkbookUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +18,8 @@ public class PriceRestController {
 
     @Autowired
     private PriceCheckService priceCheckService;
+    @Autowired
+    private MainService mainService;
 
     private Workbook priceTable;
 
@@ -35,4 +40,25 @@ public class PriceRestController {
         }
         return new byte[0];
     }
+
+    @SneakyThrows
+    @PostMapping(value = "/pricecheck/{urlIndex}/{insertIndex}")
+    public ResponseEntity<?> acceptFile(@RequestParam("file") MultipartFile file,
+                                        @PathVariable int urlIndex,
+                                        @PathVariable int insertIndex) {
+        if (isNotValid(file)) {
+            return ResponseEntity.badRequest().body("File extension should be .xls or .xlsx");
+        }
+        mainService.start(file.getBytes(), urlIndex, insertIndex);
+        return ResponseEntity.accepted().body("File: " + file.getOriginalFilename() + " with url index : " + urlIndex +
+                " and insert index: " + insertIndex + " accepted.");
+    }
+
+    public boolean isNotValid(MultipartFile file) {
+        return !file.isEmpty()
+                && file.getSize() > 0
+                && !file.getContentType().toLowerCase().equals("application/vnd.ms-excel")
+                && !file.getContentType().toLowerCase().equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    }
 }
+
---
+++ src/test/java/oleksandrdiachenko/pricechecker/PriceRestControllerTest.java
@@ -0,0 +1,90 @@
+package oleksandrdiachenko.pricechecker;
+
+import oleksandrdiachenko.pricechecker.controller.PriceRestController;
+import oleksandrdiachenko.pricechecker.service.MainService;
+import oleksandrdiachenko.pricechecker.service.PriceCheckService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static java.lang.String.format;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(PriceRestController.class)
+@EnableWebMvc
+public class PriceRestControllerTest {
+
+    public static final String XLS_CONTENT_TYPE = "application/vnd.ms-excel";
+    public static final String XLSX_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    public static final String JPG_CONTENT_TYPE = "image/jpg";
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MainService mainService;
+    @MockBean
+    private PriceCheckService priceCheckService;
+
+    @Test
+    void shouldAcceptMultipartPostRequestWhenFileIsXls() throws Exception {
+        String fileName = "filename.xls";
+        byte[] bytes = "some xls".getBytes();
+        int urlIndex = 1;
+        int insertIndex = 2;
+
+        mvc.perform(multipart(format("/pricecheck/%d/%d", urlIndex, insertIndex))
+                .file(new MockMultipartFile("file", fileName,
+                        XLS_CONTENT_TYPE, bytes)))
+                .andExpect(content().string(
+                        format("File: %s with url index : %d and insert index: %d accepted.",
+                                fileName, urlIndex, insertIndex)
+                ))
+                .andExpect(status().isAccepted());
+        verify(mainService).start(bytes, urlIndex, insertIndex);
+    }
+
+    @Test
+    void shouldAcceptMultipartPostRequestWhenFileIsXlsx() throws Exception {
+        String fileName = "filename.xlsx";
+        byte[] bytes = "some xlsx".getBytes();
+        int urlIndex = 1;
+        int insertIndex = 2;
+
+        mvc.perform(multipart(format("/pricecheck/%d/%d", urlIndex, insertIndex))
+                .file(new MockMultipartFile("file", fileName,
+                        XLSX_CONTENT_TYPE, bytes)))
+                .andExpect(content().string(
+                        format("File: %s with url index : %d and insert index: %d accepted.",
+                                fileName, urlIndex, insertIndex)
+                ))
+                .andExpect(status().isAccepted());
+        verify(mainService).start(bytes, urlIndex, insertIndex);
+    }
+
+    @Test
+    void shouldRejectMultipartPostRequestWhenFileTypeIsJpg() throws Exception {
+        String fileName = "filename.jpg";
+        byte[] bytes = "some jpg".getBytes();
+        int urlIndex = 1;
+        int insertIndex = 2;
+
+        mvc.perform(multipart(format("/pricecheck/%d/%d", urlIndex, insertIndex))
+                .file(new MockMultipartFile("file", fileName,
+                        IMAGE_JPEG_VALUE, bytes)))
+                .andExpect(content().string("File extension should be .xls or .xlsx"))
+                .andExpect(status().isBadRequest());
+        verify(mainService, times(0)).start(bytes, urlIndex, insertIndex);
+    }
+}
