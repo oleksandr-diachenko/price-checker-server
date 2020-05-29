d3d92bcb48e3cff8bb1339fafbf2320f65da7d26
oleksandr-diachenko <mail.positiv@gmail.com>
2020-05-29 13:53:50 +0300

    [Resolve #52] Migrate to postresql

---
+++ src/main/java/oleksandrdiachenko/pricechecker/DataSourceConfiguration.java
@@ -0,0 +1,30 @@
+package oleksandrdiachenko.pricechecker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
