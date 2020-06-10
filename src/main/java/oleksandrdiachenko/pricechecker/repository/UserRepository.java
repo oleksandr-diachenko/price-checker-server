c507a5358303abc29a3b5474db458bfa2749cef7
oleksandr-diachenko <mail.positiv@gmail.com>
2020-06-10 14:02:41 +0300

    [Resolve #59] Auth should ignore case sensivity in username

--- src/main/java/oleksandrdiachenko/pricechecker/repository/UserRepository.java
+++ src/main/java/oleksandrdiachenko/pricechecker/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameIgnoreCase(String username);
 
     Boolean existsByUsername(String username);
 
