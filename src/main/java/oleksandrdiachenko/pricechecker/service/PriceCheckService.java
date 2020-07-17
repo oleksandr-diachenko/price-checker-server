6bb92e4b39147675959f7f944563eb04b0d4d6db
oleksandr-diachenko <mail.positiv@gmail.com>
2020-07-17 18:15:54 +0300

    [Resolve #80] Improve check speed

--- src/main/java/oleksandrdiachenko/pricechecker/service/PriceCheckService.java
+++ src/main/java/oleksandrdiachenko/pricechecker/service/PriceCheckService.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
-import static oleksandrdiachenko.pricechecker.util.UrlUtils.getDomainName;
 import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.trim;
@@ -34,17 +33,17 @@ public class PriceCheckService {
         if (!isPositives(urlIndex, insertIndex)) {
             return excel.buildWorkBook(table);
         }
-        for (List<String> row : table) {
+        table.parallelStream().forEach(row ->  {
             for (Magazine magazine : emptyIfNull(magazines)) {
                 String url = retrieveUrl(row, urlIndex);
                 if (magazine.isThisWebsite(url)) {
-                    log.info("Searching price for {} in magazine {}", url, getDomainName(url));
+                    log.info("Searching price for {} in magazine {}", url, Magazine.class.getSimpleName());
                     String price = magazine.getPrice(magazine.getDocument(url));
                     log.info("Price for url: {} is {}", url, price);
                     insert(row, insertIndex, price);
                 }
             }
-        }
+        });
         log.info("Returning table: {}", table.toString());
         return excel.buildWorkBook(table);
     }
